08bcc26d7a17b58b87ad01f78206d589281a3240
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2023-09-29 22:27:33 +0200

    Custom Bar Chart

--- src/screens/Statistics/statisticsScreen.tsx
+++ src/screens/Statistics/statisticsScreen.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {Dimensions, Text, View} from 'react-native';
-import {LineChart, PieChart} from 'react-native-chart-kit';
+import {BarChart, LineChart, PieChart} from 'react-native-chart-kit';
 import {palette, spacing} from '../../style';
 import {getSpendings} from '../../util/storage';
 import {SpendingType} from './../../types/index';
+import GlobalBarChart from '../../components/GlobalBarChart';
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -78,6 +79,33 @@ export default function StatisticsScreen() {
         center={[0, 0]}
         absolute
       />
+      <GlobalBarChart />
+      {/* <BarChart
+        // style={{}}
+        data={data}
+        width={screenWidth}
+        height={220}
+        yAxisSuffix=" Ft"
+        // chartConfig={chartConfig}
+        chartConfig={{
+          // width: '90%',
+          backgroundColor: '#e26a00',
+          // backgroundGradientFrom: '#fb8c00',
+          // backgroundGradientTo: '#ffa726',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#ffa726',
+          },
+        }}
+        verticalLabelRotation={30}
+      /> */}
     </View>
   );
 }
