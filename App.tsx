307392dde70a1c16b1b5d2ee9fedf9c47c9c4fdc
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2023-10-02 22:13:12 +0200

    Adding app icon, install splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './src/config/navigation';
 import {Toast} from 'react-native-toast-message/lib/src/Toast';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <Navigation />
