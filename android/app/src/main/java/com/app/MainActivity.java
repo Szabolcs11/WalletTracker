e25418abfe56bf0c692e0e2787d35e2d65e5a7eb
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2023-10-04 00:54:06 +0200

    Update app name for production

--- android/app/src/main/java/com/app/MainActivity.java
+++ android/app/src/main/java/com/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.wallettracker.dev;
+package com.wallettracker;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -15,7 +15,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "WalletTracker.dev";
+    return "WalletTracker";
   }
 
   @Override
