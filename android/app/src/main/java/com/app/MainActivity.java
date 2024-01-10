3423becdb02b32d4c11ef57f7db622dc4afeca67
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-01-11 00:44:26 +0100

    Rename app to dev

--- android/app/src/main/java/com/app/MainActivity.java
+++ android/app/src/main/java/com/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.wallettracker;
+package com.wallettracker.dev;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -15,7 +15,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "WalletTracker";
+    return "WalletTracker.dev";
   }
 
   @Override
